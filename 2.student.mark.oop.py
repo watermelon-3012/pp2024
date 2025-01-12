4d4c28a1cc148c0c315daffa92e0ed44645d0fc6
watermelon-3012 <han.a3.k13tv@gmail.com>
2025-01-13 00:02:45 +0700

    labwork2

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,22 +1,27 @@
-class Student:
-    def __init__(self,id,name,dob):
-        self.__id=id
+class Person:
+    def __init__(self,name,dob):
         self.__name=name
         self.__dob=dob
-    def __str__(self):
-        return f"Student: {self.__id} - {self.__name} - DOB: {self.__dob}"
-    def getID(self):
-        return self.__id
     def getName(self):
         return self.__name
     def getDOB(self):
         return self.__dob
-    def setID(self,id):
-        self.__id=id
     def setName(self,name):
         self.__name=name
     def setDOB(self,dob):
         self.__dob=dob
+        
+class Student(Person):
+    def __init__(self,id,name,dob):
+        super().__init__(name,dob)
+        self.__id=id
+    def __str__(self):
+        return f"Student: {self.__id} - {self.__name} - DOB: {self.__dob}"
+    def getID(self):
+        return self.__id
+    def setID(self,id):
+        self.__id=id
+    
     
 class Course:
     def __init__(self,id,name):
